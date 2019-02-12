d2cc1c47716880641cb1a2b28190ec8cd478e722
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-12 17:31:16 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+require "pry"
+class Post 
+ attr_accessor :title, :author
+ @@all = []
+ 
+ def initialize(title)
+   @title = title
+   @@all << self
+ end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def title 
+    @title
+  end
+  
+  def author_name
+    binding.pry
+    self.author.name
+  end
+  
+  
+end
